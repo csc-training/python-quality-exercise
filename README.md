1315fd5e81b14440a5e1a7a3c3e1d797bcaf5f2b
Jyry Suvilehto <jyry.suvilehto@csc.fi>
2017-01-03 13:53:18 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Demo project for Python-Introduction course
+
+This project resembles a small, untested library for Python that has the
+following caveats
+
+1. no setup.py
+
+2. no unit tests for code (of which there isn't much)
+  * code intentionally has bugs
+3. no style checking done
+4. different behaviour in different Python versions (so testing with tox might
+be a good idea)
+
---
+++ demolib/demomodule.py
@@ -0,0 +1,33 @@
+""" Python Demo module
+
+Contains functions which are buggy or not generic
+"""
+def find_median(list_)
+    """ returns the statistical median from list
+    """
+    # bugs: does not sort list
+    # idx will be a float on Python 3
+    # using sorted() function is a good idea as that doesn't modify the
+    # original list
+    idx=len(list_)/2
+    return list_[idx]
+
+
+def say_hello(hello):
+    """ prints a statement """
+    # breaks in python 3 as print is a function in python3
+    print hello
+
+class DemoClass(object):
+    """ A simple class that has an unwieldy constructor
+    """
+    def __init__(self):
+        #this should take a while to compute on most modern computers
+        #also indentation is wonky
+        self._value = sum(
+    range(10000000000
+            ))
+
+    def add(self, value)
+        """ return stored value plus given value """
+        return self._value + value
