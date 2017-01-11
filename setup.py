5f94a87457ff6202ba6baf035c5ea8073fe47f10
Jyry Suvilehto <jyry.suvilehto@csc.fi>
2017-01-11 14:20:28 +0200

    example tox.ini. TODO example tests and code quality fixes.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Demolib',
+      version='0.1',
+      description='Python Tools demonstration library',
+      author='Jyry Suvilehto',
+      author_email='suvileht@csc.fi',
+      url='https://github.com/csc-training/python-quality-exercise',
+      packages=['demolib'],
+     )
